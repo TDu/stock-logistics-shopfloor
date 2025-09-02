d4abacf9cdd6002b9323661ab37abc11bd758a61
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-09-02 08:29:25 +0200

    fixup! Mobile - add storage type

--- shopfloor_reception_mobile/static/src/scenario/reception.esm.js
+++ shopfloor_reception_mobile/static/src/scenario/reception.esm.js
@@ -17,7 +17,7 @@ const Reception = {
                 <state-display-info :info="state.display_info" v-if="state.display_info"/>
             </template>
             <searchbar
-                v-if="state_in(['select_document', 'select_move', 'set_lot', 'set_quantity', 'set_destination', 'select_dest_package'])"
+                v-if="state_in(['select_document', 'select_move', 'set_lot', 'set_quantity', 'set_destination', 'select_dest_package', 'set_storage_type'])"
                 v-on:found="on_scan"
                 :input_placeholder="search_input_placeholder"
             />
@@ -161,7 +161,9 @@ const Reception = {
                     </div>
                 </div>
             </template>
-            <template v-if="state_is('set_destination')">
+
+
+            <template v-if="state_is('set_storage_type')">
                 <item-detail-card
                     :record="line_being_handled"
                     :options="picking_detail_options_for_set_destination()"
@@ -170,7 +172,31 @@ const Reception = {
                 />
                 <item-detail-card
                     :record="line_being_handled"
-                    :options="storage_type_options(line_being_handled)"
+                    :options="storage_type_options(line_being_handled, false)"
+                    :card_color="utils.colors.color_for('screen_step_todo')"
+                    :key="make_state_component_key(['reception-product-item-detail-set-destination-pack-type', state.data.picking.id])"
+                />
+                <div class="button-list button-vertical-list full">
+                    <v-row align="center">
+                        <v-col class="text-center" cols="12">
+                            <btn-back/>
+                        </v-col>
+                    </v-row>
+                </div>
+            </template>
+
+
+            <template v-if="state_is('set_destination')">
+                <item-detail-card
+                    :record="line_being_handled"
+                    :options="picking_detail_options_for_set_destination()"
+                    :card_color="utils.colors.color_for('screen_step_done')"
+                    :key="make_state_component_key(['reception-product-item-detail-set-destination-pack', state.data.picking.id])"
+                />
+                <item-detail-card
+                    v-if="line_being_handled.package_dest"
+                    :record="line_being_handled"
+                    :options="storage_type_options(line_being_handled, true)"
                     :card_color="utils.colors.color_for('screen_step_todo')"
                     :key="make_state_component_key(['reception-product-item-detail-set-destination-pack-type', state.data.picking.id])"
                 />
@@ -423,15 +449,28 @@ const Reception = {
 
 
         storage_type_select: function () {
-            alert("What's up ?");
+            this.wait_call(
+                this.odoo.call("set_storage_type", {
+                    picking_id: this.state.data.picking.id,
+                    selected_line_id: this.line_being_handled.id,
+                    barcode: "",
+                    })
+                );
         },
-        storage_type_options: function (line) {
-            return  {
+        storage_type_options: function (line, withAction=false) {
+            const options = {
                 key_title: "package_dest.storage_type.name",
                 title_default: "?",
+            };
+            const optionsAction = {
                 title_action_icon: "mdi-help-circle",
                 on_title_action: this.storage_type_select,
             };
+
+            if (withAction==true) {
+                return Object.assign(options, optionsAction);
+            }
+            return options;
         },
 
 
